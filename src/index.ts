cd147f1e8c4db730febaf4682d054ff56911025b
Ko-GyeongTae <kokt0203@naver.com>
2021-06-18 15:03:01 +0900

    <EDIT> Logic

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { crawl, profile } from './lib/crawl';
 import schedule from 'node-schedule';
-import mongoose from 'mongoose';
+import mongoose, { Error } from 'mongoose';
 
 const mongoConfig = {
     useNewUrlParser: true,
@@ -8,13 +8,21 @@ const mongoConfig = {
     useUnifiedTopology: true
 };
 
-
+console.log('Start to run crawler...');
 schedule.scheduleJob('00 * * * * *', async () => {
     console.log("crwalStart");
-    await mongoose.connect(process.env.MONGO, mongoConfig, () => {
-        console.log("Connect to Database");
-    });
-    await profile();
-    await crawl();
+    try {
+        await mongoose.connect(process.env.MONGO, mongoConfig, () => {
+            console.log("Connect to Database");
+        });
+        try {
+            await profile();
+            await crawl();
+        } catch (e) {
+            throw new Error(e);
+        }
+    } catch (e) {
+        throw new Error(e);
+    }
     console.log('end');
 });
--- src/lib/crawl.ts
+++ src/lib/crawl.ts
@@ -9,50 +9,45 @@ const headers = {
     "User-Agent": "PostmanRuntime/7.28.0"
 }
 const Alloptions = {
-    url : 'https://api.github.com/users/Ko-GyeongTae/repos',
-    method:'GET',
+    url: 'https://api.github.com/users/Ko-GyeongTae/repos',
+    method: 'GET',
     headers: headers,
 };
 
 const Profileoptions = {
-    url : 'https://api.github.com/users/Ko-GyeongTae',
-    method:'GET',
+    url: 'https://api.github.com/users/Ko-GyeongTae',
+    method: 'GET',
     headers: headers,
 }
 
 //'https://api.github.com/orgs/OS-2021/repos'
 
 export const crawl = () => {
+    console.log('Start to get repositories...');
     request(Alloptions, (err, res, body) => {
-       if(err) console.log(err);
-       let result = JSON.parse(body);
-       result.map(info => {
-           const repo = new Repo(info)
-           repo.save(err => {
-               if(err) console.log(err);
-               else console.log(repo);
-           });
-       })
-    })
-};
-
-export const profile = (resp?):any => {
-    request(Profileoptions, (err, res, body) => {
-        if(err) console.log(err);
-        console.log(JSON.parse(body));
-        if (resp) resp.send(JSON.parse(body));
-        return JSON.parse(body);
-    })
-}
-
-
-/*
-export const crawl = () => {
-    new Promise<string>((resolve, reject) => {
-        request.get('https://github.com/Ko-GyeongTae?tab=repositories&q=&type=public&language=&sort=name', (err, res) => {
-            if(err) reject(err);
-            resolve(res.body);
+        if (err) console.log(err);
+        let result = JSON.parse(body);
+        result.map(info => {
+            const repo = new Repo(info);
+            repo.save(err => {
+                if (err) console.error(err);
+                else console.log(repo.name);
+            });
         });
     });
+    console.log('End to get repositories...');
 };
-*/
+
+export const profile = () => {
+    console.log('Start to get profile...');
+    request(Profileoptions, (err, res, body) => {
+        if (err) console.log(err);
+        let result = JSON.parse(body);
+        const profile = new Profile(result);
+        profile.save(err => {
+            if (err) console.error(err);
+            else console.log(profile);
+        });
+    });
+    console.log('End to get profile');
+}
